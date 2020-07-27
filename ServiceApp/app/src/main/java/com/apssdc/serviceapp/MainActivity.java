4c50f978e03e71c4cc781771588d0bb5cffd055d
krishna <svkrishna.p@apssdc.in>
2020-07-27 15:46:57 +0530

    intnt

---
+++ ServiceApp/app/src/main/java/com/apssdc/serviceapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.apssdc.serviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    /*is my screen is visible
+    *  All of done create a project  goto  xml file*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view) {
+    }
+
+    public void stop(View view) {
+    }
+}
